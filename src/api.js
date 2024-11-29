c40032641b6c3f8d0972151e9c0522f5fcd62162
rayane rostane <rayane.rostane@universite-paris-saclay.fr>
2024-11-29 23:02:18 +0100

    maj de l'url de l'api

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://gestionnaire-de-cv-api.onrender.com/api";
 
 // Récupérer les recommandations pour un CV
 export const getRecommendations = async (cvId) => {
